864ee129c754200f9c5787e3fcc798e7b053ebcb
Mesele Shishay <messeleshishaymm@gmail.com>
2025-03-18 07:30:04 -0300

    config updated

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mpesa PHP SDK</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Mesele-shishay/PhpMpesaSDK",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/Mesele-shishay/PhpMpesaSDK",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Mpesa PHP SDK",
   },
-}
+};
 
-export default config
+export default config;
