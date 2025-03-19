f3c847205cd4255d8644cfc50234bf9264d36f92
Mesele Shishay <messeleshishaymm@gmail.com>
2025-03-19 03:34:13 -0300

    Add default option to show copy code in Nextra theme configuration for improved usability.

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const require = createRequire(import.meta.url);
 import nextra from "nextra";
 
 const withNextra = nextra({
+  defaultShowCopyCode: true,
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
